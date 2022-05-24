fb591e9c45e5379d0e3dade22cb8882ab41466f9
Anthony Sciamanna <asciamanna@gmail.com>
2022-05-24 10:36:59 -0400

    updates to java version - removing workitem

--- java/src/main/java/User.java
+++ java/src/main/java/User.java
@@ -9,17 +9,13 @@ public class User {
     private final String firstName;
     private final String lastName;
     private final LocalDate birthdate;
-    private final List<WorkItem> workItems;
+
 
     public User(String firstName, String lastName, LocalDate birthdate) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthdate = birthdate;
-        workItems = new ArrayList<WorkItem>();
     }
 
-    public void addWorkItem(WorkItem workItem) {
-        workItems.add(workItem);
-    }
 }
 
