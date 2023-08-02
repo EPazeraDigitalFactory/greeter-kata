24e2b446902e73ba0cd07bb87ad3b5409c0386c3
Sensei <eat@joes.com>
2023-08-02 17:26:22 +0000

    we're not all idiots!

--- csharp/Greeter-Kata.Tests/Greeter_Greet_should.cs
+++ csharp/Greeter-Kata.Tests/Greeter_Greet_should.cs
@@ -7,9 +7,18 @@ public class Greeter_Greet_should
     [InlineData("Mike","Rotch")]
     public void greet_user_by_name(string firstName, string lastName)
     {
-
-        IGreeter subject = new Greeter();
+        var timeProvider = new FakeTimeProvider(new DateTime(2020,3,12,9,0,0));
+        IGreeter subject = new Greeter(timeProvider);
         var givenUser = new User(firstName, lastName,new DateTime(1970,1,1));
         subject.Greet(givenUser).ShouldBe($"Good Morning, {firstName} {lastName}!");
+    }
+
+    [Fact]
+    public void say_good_afternoon_when_it_is_afternoon()
+    {
+        var timeProvider = new FakeTimeProvider(new DateTime(2020,3,12,14,0,0));
+        IGreeter subject = new Greeter(timeProvider);
+        var givenUser = new User("Hugh","Jass",new DateTime(1970,1,1));
+        subject.Greet(givenUser).ShouldBe($"Good Afternoon, Hugh Jass!");
     }
 }
--- csharp/Greeter-Kata/Greeter.cs
+++ csharp/Greeter-Kata/Greeter.cs
@@ -1,7 +1,21 @@
 public class Greeter : IGreeter
 {
+    private ITimeProvider _timeProvider;
+    public Greeter(ITimeProvider timeProvider)
+    {
+        _timeProvider=timeProvider;
+    }
     public string Greet(User user)
     {
-        return $"Good Morning, {user.FirstName} {user.LastName}!";
+        var rightNow = _timeProvider.Now;
+        var hour = rightNow.Hour;
+        if(hour<12)
+        {
+            return $"Good Morning, {user.FirstName} {user.LastName}!";
+        }
+        else
+        {
+            return $"Good Afternoon, {user.FirstName} {user.LastName}!";
+        }
     }
 }
