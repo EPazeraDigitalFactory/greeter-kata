326e664e5a767f9906f3d0288cbe12a81ea82ce5
Sensei <eat@joes.com>
2023-08-02 17:09:09 +0000

    details are hard

--- csharp/Greeter-Kata.Tests/Greeter_Greet_should.cs
+++ csharp/Greeter-Kata.Tests/Greeter_Greet_should.cs
@@ -4,11 +4,12 @@ public class Greeter_Greet_should
 {
     [Theory]
     [InlineData("Hugh","Jass")]
+//    [InlineData("Mike","Rotch")]
     public void greet_user_by_name(string firstName, string lastName)
     {
 
         IGreeter subject = new Greeter();
         var givenUser = new User(firstName, lastName,new DateTime(1970,1,1));
-        subject.Greet(givenUser).ShouldBe("Good Morning, Hugh Jass!");
+        subject.Greet(givenUser).ShouldBe($"Good Morning, {firstName} {lastName}!");
     }
 }
